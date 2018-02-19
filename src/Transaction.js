88b6df2189f917c9a2d1c000ec21512e4a38b47c
Del <del.larsen@gmail.com>
2018-02-19 14:14:06 -0600

    Created Trans class

---
+++ src/Transaction.js
@@ -0,0 +1,17 @@
+
+
+
+class Transaction {
+    constructor(
+        transType,
+        typeId,
+        eggCount
+    )
+    {
+        this.transType = transType; 
+        this.typeId = typeId;
+        this.eggCount = eggCount
+    }
+}
+
+export default Transaction;
