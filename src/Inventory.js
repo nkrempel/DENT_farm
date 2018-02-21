2cc3f2e64e96533b736672494c9630c6fb0f343a
Del <del.larsen@gmail.com>
2018-02-21 14:22:25 -0600

    Auto stash before merge of "master" and "origin/master"

--- src/Inventory.js
+++ src/Inventory.js
@@ -6,11 +6,12 @@ import {
   NavLink,
 } from 'react-router-dom';
 import './App.css';
+import {fetchTransactions} from './state/actions'
 import { connect } from 'react-redux';
 import axios from 'axios'
 import {BarChart} from 'react-easy-chart'
-import InfiniteCalendar from 'react-infinite-calendar';
-import {Bar, Line } from 'react-chartjs-2';
+import moment from 'moment';
+
 
 const mockAPI = 'http://5a8b1a993d92490012370bca.mockapi.io/'
 
@@ -20,189 +21,73 @@ class Inventory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inventory: [],
-      inputDate: '',
-        chartData: {
-          labels: [],
-          dataset: [],
-        },
-        // backgroundColor
-
+      startDate: '',
+      endDate: '',
     }
   }
-  
-  
-  
-  componentDidMount(){
-    this.getEggs()
-  }
-  
 
-  getEggs(){
-    axios.get(`${mockAPI}transactions`).then(({data}) => 
-    { 
-      this.setState({inventory : data }) 
-    } 
-  ) 
-}
+  componentDidMount(){
+    this.props.fetchTransactions()
+  }
+
 
 render() {
   let chartData = [] 
-  this.state.inventory.map ( item => { 
-    chartData.push ({ x: convertDate(item.createdAt), y:item.eggCount}) 
-    
+  this.props.transactions.map ( item => { 
+    chartData.push ({ x: moment.unix(item.createdAt).format('D-MMM-YY'), y:item.eggCount}) 
+    // console.log(moment.unix(item.createdAt))
   }) 
 
 // var today = new Date();
 // var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
 
-  console.log(chartData)
+  console.log(this.state)
   return <div>
       <div className="card">
       <h1 className="card">Inventory</h1>
       <div className="center">
-        {/* <input onChange={(e) => {this.setState({inputDate: e.target.value})}} type="date"/> */}
+        <label id="startDate">Start Date</label>
+        <input id='startDate' onChange={(e) => {this.setState({startDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/> 
+        <label id="endDate">End Date</label>
+        <input id='endDate' onChange={(e) => {this.setState({endDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/> 
         {/* <InfiniteCalendar width={600} height={200} selected={today} disabledDays={[0, 6]} minDate={lastWeek} /> */}
       </div>
+        
         <p>Last 7 days</p>
-        <BarChart axisLabels={{ x: 'My x Axis', y: 'My y Axis' }} axes colorBars height={250} width={650} barWidth={20} xTickNumber={5} yTickNumber={3} xType={'time'} 
-        xDomainRange={['17-Feb-18', '20-Feb-18']} data={chartData} />
+        <BarChart 
+              axisLabels={{ x: 'My x Axis', y: 'My y Axis' }} 
+              axes 
+              colorBars 
+              height={250} 
+              width={650} 
+              barWidth={20} 
+              xTickNumber={5} 
+              yTickNumber={3} 
+              datePattern="%d-%b-%y"
+              xType={'time'} 
+              xDomainRange={[this.state.startDate, this.state.endDate]} 
+              data={chartData} />
      
 
-        {/* graphJS */}
-      {/* <Bar 
-        data={chartData}
-        width={100}
-        height={50}
-        options={{
-          maintainAspectRatio: true
-        }}
-        /> */}
+  
       </div>
       
     </div>
   } 
 } 
 
-const mapStateToProps = ({ transactions }) => {
+const mapStateToProps = state => {
   return {
-    transactions: transactions
+    transactions: state.transactions
   };
 };
 
 
 const mapDispatchToProps = dispatch => {
   return {
-    getEggs: function() {
-      dispatch(this.getEggs())
-    }
+    fetchTransactions: () => dispatch(fetchTransactions())
   };
 };
-
-let currentTime = new Date();
-const currentDate =  new Date (dateFormat)
-
-const dateFormat = date => {
-  console.log(date)
-  let myDate = new Date(date)
-  console.log(myDate)
-  let dd = myDate.getDate() 
-  let mm= myDate.getMonth()+1; 
-  const yy = myDate.getFullYear() - 2000 
-  console.log(`${mm} ${dd} ${yy}`)
-  let newMM = '' 
-  
-  if(dd<10) 
-  { 
-    dd=`0${dd}` 
-  } 
-  
-
-  switch(mm){ 
-    case 1: 
-    newMM = 'Jan' 
-    case 2: 
-    newMM = 'Feb' 
-    case 3: 
-    newMM = 'Mar' 
-    case 4: 
-    newMM = 'Apr' 
-    case 5: 
-    newMM = 'May' 
-    case 6: 
-    newMM = 'Jun' 
-    case 7: 
-    newMM = 'Jul' 
-    case 8: 
-    newMM = 'Aug' 
-    case 9: 
-    newMM = 'Sep' 
-    case 10: 
-    newMM = 'Oct' 
-    case 11: 
-    newMM = 'Nov' 
-    case 12: 
-    newMM = 'Dec' 
-    console.log(mm)
-
-  } 
-  
-  let reformatedDate =`${dd}-${newMM}-${yy}` 
-  console.log(reformatedDate)
-  return reformatedDate 
-} 
-
-
-const convertDate = date => { 
-  let myDate = new Date(date * 1000) 
-  let dd = myDate.getDate() 
-  let mm= myDate.getMonth()+1; 
-  const yy = myDate.getFullYear() - 2000 
-  let newMM = '' 
-  
-  if(dd<10) 
-  { 
-    dd=`0${dd}` 
-  } 
-  
-  // if(mm<10) 
-  // { 
-  //   mm=`0${mm}` 
-  // } 
-  switch(mm){ 
-    case 1: 
-    newMM = 'Jan' 
-    case 2: 
-    newMM = 'Feb' 
-    case 3: 
-    newMM = 'Mar' 
-    case 4: 
-    newMM = 'Apr' 
-    case 5: 
-    newMM = 'May' 
-    case 6: 
-    newMM = 'Jun' 
-    case 7: 
-    newMM = 'Jul' 
-    case 8: 
-    newMM = 'Aug' 
-    case 9: 
-    newMM = 'Sep' 
-    case 10: 
-    newMM = 'Oct' 
-    case 11: 
-    newMM = 'Nov' 
-    case 12: 
-    newMM = 'Dec' 
-    default: 
-    newMM = "Feb"
-  } 
-  
-  let reformatedDate =`${dd}-${newMM}-${yy}` 
-  console.log(reformatedDate)
-  return reformatedDate 
-} 
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Inventory);
