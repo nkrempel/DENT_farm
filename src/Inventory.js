e6d65cb6c96ab6f717a88d963477af18a7922def
Del Larsen <del.larsen@gmail.com>
2018-02-22 16:45:19 -0600

    Added total inventory on hand

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,46 +1,74 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Link, Route, NavLink } from 'react-router-dom'
-import './App.css'
-import { fetchTransactions } from './state/actions'
-import { connect } from 'react-redux'
-import axios from 'axios'
-import { BarChart } from 'react-easy-chart'
-import moment from 'moment'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import './App.css';
+import { fetchTransactions } from './state/actions';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { BarChart } from 'react-easy-chart';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const mockAPI = 'http://5a8b1a993d92490012370bca.mockapi.io/'
+const mockAPI = 'http://5a8b1a993d92490012370bca.mockapi.io/';
 
 class Inventory extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       startDate: '',
       endDate: '',
-      chartData: []
+      chartData: [],
       // eggTypeSource: '',
-    }
-    this.handleChangeStart = this.handleChangeStart.bind(this)
-    this.handleChangeEnd = this.handleChangeEnd.bind(this)
+      showToolTip: '',
+    };
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
+    this.mouseOverHandler = this.mouseMoveHandler.bind(this);
+    this.mouseMoveHandler = this.mouseMoveHandler.bind(this);
+    this.mouseOutHandler = this.mouseOutHandler.bind(this);
     // this.handleOptionChange = this.handleOptionChange.bind(this)
   }
 
-  componentDidMount () {
-    this.props.fetchTransactions()
+  componentDidMount() {
+    this.props.fetchTransactions();
   }
 
-  handleChangeStart (date) {
+  handleChangeStart(date) {
     // this.setState({startDate: moment(date).format('MM-DD-YYYY')
     this.setState({
-      startDate: moment(date)
-    })
+      startDate: moment(date),
+    });
   }
-  handleChangeEnd (date) {
+  handleChangeEnd(date) {
     // this.setState({endDate: moment(date).format('D-MMM-YY')
     this.setState({
-      endDate: date
-    })
+      endDate: date,
+    });
   }
+  mouseOverHandler(d, e) {
+    this.setState({
+      showToolTip: true,
+      top: `${e.screenY - 10}px`,
+      left: `${e.screenX + 10}px`,
+      y: d.y,
+      x: d.x,
+    });
+  }
+
+  mouseMoveHandler(e) {
+    if (this.state.showToolTip) {
+      this.setState({ top: `${e.y - 10}px`, left: `${e.x + 10}px` });
+    }
+  }
+  mouseOutHandler() {
+    this.setState({ showToolTip: false });
+  }
+
   // datePickerChecker(){
   //   if(this.state.startDate === this.state.endDate){
   //     console.log("date enabled")
@@ -53,57 +81,92 @@ class Inventory extends Component {
   //   }
   //   }
 
-  render () {
-    let chartData = []
-    let sortData = this.props.transactions.slice()
-    sortData.sort(function (a, b) {
-      return a.transactionDate - b.transactionDate
-    })
-    let currentDate = 0
-    let firstPass = true
-    let dateEggCount = 0
-    console.log(sortData)
-    let sortDataLength = sortData.length
+  render() {
+    let totalCollection = 0;
+    let totalCompletedOrders = 0;
+    let openOrders = 0;
+
+    this.props.transactions.map(trans => {
+      if (trans.transType === 'Collect') {
+        totalCollection += trans.eggCount;
+      } else {
+        totalCompletedOrders += trans.eggCount;
+      }
+    });
+
+    console.log(`totalCollection - ${totalCollection} `)
+    console.log(`totalCompletedOrders - ${totalCompletedOrders} `)
+    
+
+
+    this.props.orders.map(order => {
+      if (order.status === 'Open') {
+        openOrders += parseInt(order.count);
+      }
+    });
+
+    console.log(`openOrders - ${openOrders}`)
+    let onHandInventory = totalCollection - totalCompletedOrders;
+    let availableInventory = onHandInventory - openOrders;
+    let chartData = [];
+    let sortData = this.props.transactions.slice();
+    sortData.sort(function(a, b) {
+      return a.transactionDate - b.transactionDate;
+    });
+    let currentDate = 0;
+    let firstPass = true;
+    let dateEggCount = 0;
+    console.log(sortData);
+    let sortDataLength = sortData.length;
 
     sortData.map((item, idx) => {
       if (item.transType === 'Collect') {
         if (firstPass) {
-          currentDate = moment.unix(item.transactionDate).format('D-MMM-YY')
-          firstPass = false
-          dateEggCount = dateEggCount + item.eggCount
-          console.log('first pass current count ' + dateEggCount)
+          currentDate = moment.unix(item.transactionDate).format('D-MMM-YY');
+          firstPass = false;
+          dateEggCount = dateEggCount + item.eggCount;
+          // console.log('first pass current count ' + dateEggCount);
         } else if (
           currentDate === moment.unix(item.transactionDate).format('D-MMM-YY')
         ) {
-          dateEggCount = dateEggCount + item.eggCount
-          console.log('current count ' + dateEggCount)
+          dateEggCount = dateEggCount + item.eggCount;
+          // console.log('current count ' + dateEggCount);
           if ((idx = sortDataLength - 1)) {
             chartData.push({
               x: currentDate,
-              y: dateEggCount
-            })
+              y: dateEggCount,
+            });
           }
         } else {
-          console.log('this was pushed ' + currentDate + ': ' + dateEggCount)
+          // console.log('this was pushed ' + currentDate + ': ' + dateEggCount);
           chartData.push({
             x: currentDate,
-            y: dateEggCount
-          })
-          dateEggCount = item.eggCount
-          currentDate = moment.unix(item.transactionDate).format('D-MMM-YY')
+            y: dateEggCount,
+          });
+          dateEggCount = item.eggCount;
+          currentDate = moment.unix(item.transactionDate).format('D-MMM-YY');
         }
-        console.log(chartData)
+        // console.log(chartData);
       }
-    })
+    });
 
     // console.log(this.state)
-    console.log(this.props.transactionDate)
+    console.log(this.props.transactionDate);
+
     return (
       <div>
-        <div className='card'>
-          <h1 className='card'>Inventory</h1>
+        <div className="card">
+          <h1 className="card">Inventory</h1>
+        <div className="text-left">
+          <h4>Total Onhand Inventory</h4>
+          <p> {onHandInventory}</p>
+        </div>
+        <div className="text-right">
 
-          <div className='center'>
+          <h4>Available Inventory "Less pending orders"</h4>
+          <p>{availableInventory}</p>
+        </div>
+          <div className="center">
             {/* <label id="startDate">Start Date</label>
         <input id='startDate' onChange={(e) => {this.setState({startDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/>
         <label id="endDate">End Date</label>
@@ -115,7 +178,7 @@ class Inventory extends Component {
               endDate={this.state.endDate}
               // disabled={this.datePickerChecker}
               onChange={this.handleChangeStart}
-              placeholderText='Start Date'
+              placeholderText="Start Date"
             />
 
             <DatePicker
@@ -125,9 +188,8 @@ class Inventory extends Component {
               endDate={this.state.endDate}
               onChange={this.handleChangeEnd}
               // disabled={this.datePickerChecker}
-              placeholderText='End Date'
+              placeholderText="End Date"
             />
-
           </div>
 
           <p>Eggs Per Day</p>
@@ -140,32 +202,37 @@ class Inventory extends Component {
             barWidth={20}
             xTickNumber={5}
             yTickNumber={5}
-            datePattern='%d-%b-%y'
+            datePattern="%d-%b-%y"
             xType={'time'}
             xDomainRange={[
               moment(this.state.startDate).format('D-MMM-YY'),
-              moment(this.state.endDate).format('D-MMM-YY')
+              moment(this.state.endDate).format('D-MMM-YY'),
             ]}
             data={chartData}
+            clickHandler={d =>
+              this.setState({
+                dataDisplay: `The value on the ${d.x} is ${d.y}`,
+              })
+            }
+            yDomainRange={[0, 25]}
           />
-
         </div>
-
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    transactions: state.transactions
-  }
-}
+    orders: state.orders,
+    transactions: state.transactions,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchTransactions: () => dispatch(fetchTransactions())
-  }
-}
+    fetchTransactions: () => dispatch(fetchTransactions()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Inventory)
+export default connect(mapStateToProps, mapDispatchToProps)(Inventory);
