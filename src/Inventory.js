89f2513eadb783c5ef52d7ff0f6e71f8a338e28d
Del <del.larsen@gmail.com>
2018-02-21 21:52:32 -0600

    date picker and graph work!

--- src/Inventory.js
+++ src/Inventory.js
@@ -11,6 +11,8 @@ import { connect } from 'react-redux';
 import axios from 'axios'
 import {BarChart} from 'react-easy-chart'
 import moment from 'moment';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
 
 
 const mockAPI = 'http://5a8b1a993d92490012370bca.mockapi.io/'
@@ -22,14 +24,38 @@ class Inventory extends Component {
     super(props);
     this.state = {
       startDate: '',
-      endDate: new Date() ,
+      endDate: '',
     }
+    this.handleChangeStart = this.handleChangeStart.bind(this)
+    this.handleChangeEnd = this.handleChangeEnd.bind(this)
   }
 
   componentDidMount(){
     this.props.fetchTransactions()
   }
 
+  handleChangeStart(date){
+    // this.setState({startDate: moment(date).format('MM-DD-YYYY')
+    this.setState({startDate: moment(date)
+    })
+  }
+  handleChangeEnd(date){
+    // this.setState({endDate: moment(date).format('D-MMM-YY')
+    this.setState({endDate: date
+    })
+  }
+  // datePickerChecker(){
+  //   if(this.state.startDate === this.state.endDate){
+  //     console.log("date enabled")
+  //     return "false"
+  //   } else if (this.state.startDate < this.state.endDate){
+  //     console.log("date disabled")
+  //     return "false"
+  //   } else {
+  //     return "true"
+  //   }
+  //   }
+  
 
 render() {
   let chartData = [] 
@@ -38,8 +64,6 @@ render() {
     // console.log(moment.unix(item.createdAt))
   }) 
 
-// var today = new Date();
-// var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
 
   console.log(this.state)
   return <div>
@@ -48,11 +72,30 @@ render() {
      
      
       <div className="center">
-        <label id="startDate">Start Date</label>
+        {/* <label id="startDate">Start Date</label>
         <input id='startDate' onChange={(e) => {this.setState({startDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/> 
         <label id="endDate">End Date</label>
-        <input id='endDate' defaultValue="2018-02-21" onChange={(e) => {this.setState({endDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/> 
-       
+        <input id='endDate' defaultValue="2018-02-21" onChange={(e) => {this.setState({endDate: moment(e.target.value).format('D-MMM-YY')})}} type="date"/>  */}
+      <DatePicker
+        selected={this.state.startDate}
+        selectsStart
+        startDate={this.state.startDate}
+        endDate={this.state.endDate}
+        // disabled={this.datePickerChecker}
+        onChange={this.handleChangeStart}
+        placeholderText="Start Date"
+    />
+
+    <DatePicker
+        selected={this.state.endDate}
+        selectsEnd
+        startDate={this.state.startDate}
+        endDate={this.state.endDate}
+        onChange={this.handleChangeEnd}
+        // disabled={this.datePickerChecker}
+        placeholderText="End Date"
+    />
+  
       </div>
   
         <p>Eggs Per Day</p>
@@ -67,7 +110,7 @@ render() {
               yTickNumber={3} 
               datePattern="%d-%b-%y"
               xType={'time'} 
-              xDomainRange={[this.state.startDate, this.state.endDate]} 
+              xDomainRange={[moment(this.state.startDate).format('D-MMM-YY'), moment(this.state.endDate).format('D-MMM-YY')]} 
               data={chartData} />
 
       </div>
